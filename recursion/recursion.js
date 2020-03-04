e1ebfb7fc202f6e1b089fe0714ea4da4bc906e47
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2020-03-04 11:23:57 -0500

    Rest paramters + spread syntax

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -136,7 +136,33 @@ function printReverseList(list){
 printReverseList(list);
 printReverseList(secondList);
 
+// Rest parameters syntax
+function showName(firstName, lastName, ...titles) {
+  alert( firstName + ' ' + lastName ); // Julius Caesar
 
+  // the rest go into titles array
+  // i.e. titles = ["Consul", "Imperator"]
+  alert( titles[0] ); // Consul
+  alert( titles[1] ); // Imperator
+  alert( titles.length ); // 2
+}
 
+showName("Julius", "Caesar", "Consul", "Imperator");
 
+// Spread syntax
+function showName(firstName, lastName, ...titles) {
+  alert( firstName + ' ' + lastName ); // Julius Caesar
 
+  // the rest go into titles array
+  // i.e. titles = ["Consul", "Imperator"]
+  alert( titles[0] ); // Consul
+  alert( titles[1] ); // Imperator
+  alert( titles.length ); // 2
+}
+
+showName("Julius", "Caesar", "Consul", "Imperator");
+
+let str = "Hello";
+
+alert( [...str] ); // H,e,l,l,o
+alert( Array.from(str) ); // H,e,l,l,o
